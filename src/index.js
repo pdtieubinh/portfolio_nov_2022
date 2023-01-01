9723c92efae729a4b9a721d0457aee87a7d21946
pdtieubinh <tinybottles2112@gmail.com>
2023-01-01 22:29:39 +0200

    happy new year

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Portfolio app, last updated: 01.01.2023
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
