90afcd8c22e6e878a2d91143ab47b7b7d4dcb197
pdtieubinh <tinybottles2112@gmail.com>
2023-01-07 01:48:01 +0200

    popper

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,21 +1,42 @@
 import './Content.css';
 import { useState } from "react"
 import { InfoPanel } from './InfoPanel';
+import Popper from '@mui/material/Popper';
 
 // Full content to be used elsewhere
 export default function Content({ type, script }) {
-    const [hovered, setHovered] = useState(false)
 
-    // Calculate everythin on hover
-    const hover = () => { setHovered(true) }
-    const unhover = () => { setHovered(false) }
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const hover = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const unhover = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popper' : undefined;
+
+    // const [hovered, setHovered] = useState(false)
+
+    // // Calculate everythin on hover
+    // const hover = () => { setHovered(true) }
+    // const unhover = () => { setHovered(false) }
 
     return (
+        <>
         <div 
         className={`Content ${ (['certified exams', 'mooc'].includes(type.toLowerCase())) ? 'ContentCerti': ''}`}
         onMouseEnter={hover} onMouseLeave={unhover}>
             <img alt={`${script.id} logo`} src={ script.link }/>
-            { (hovered) ? <InfoPanel type={type} script={script}/> : null }
+            {/* (hovered) ? <InfoPanel type={type} script={script}/> : null */}
+            <Popper id={id} open={open} anchorEl={anchorEl}>
+                <InfoPanel type={type} script={script}/>
+            </Popper>
         </div>
+        
+    </>
     )
 }
--- src/components/content/InfoPanel.js
+++ src/components/content/InfoPanel.js
@@ -1,33 +1,34 @@
-import { useEffect, useRef, useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
 import './Content.css';
 import { CerInfo, ExpInfo, EduInfo, MooInfo } from './textInfo';
 
 // The popup panel (tooltip) containing text info
 export const InfoPanel = ({ type, script }) => {
 
-    const marginRight = 10
+    // const marginRight = 10
 
-    const [left, setLeft] = useState('auto')
+    // const [left, setLeft] = useState('auto')
 
-    const ref = useRef(null)
+    // const ref = useRef(null)
 
-    useEffect(() => {
-        // Get current values
-        const infoLeft = ref.current.offsetLeft
-        const infoWidth = ref.current.offsetWidth
-        const windowWidth = window.outerWidth // not inner width, which is dependent by children's overflow
+    // useEffect(() => {
+    //     // Get current values
+    //     const infoLeft = ref.current.offsetLeft
+    //     const infoWidth = ref.current.offsetWidth
+    //     const windowWidth = window.outerWidth // not inner width, which is dependent by children's overflow
         
-        // Calculate new values
-        if (infoLeft + infoWidth >= windowWidth + marginRight) {
-            setLeft(`${windowWidth - infoWidth - marginRight}px`)
-        }
-    }, [])
+    //     // Calculate new values
+    //     if (infoLeft + infoWidth >= windowWidth + marginRight) {
+    //         setLeft(`${windowWidth - infoWidth - marginRight}px`)
+    //     }
+    // }, [])
 
     return (
         <div 
         className='InfoPanel' 
-        ref={ref} 
-        style={{'left': left}}>
+        //ref={ref} 
+        //style={{'left': left}}
+        >
             {(type.toLowerCase() === 'certified exams') ? <CerInfo script={script}/>
             : (type.toLowerCase() === 'experiences') ? <ExpInfo script={script}/>
             : (type.toLowerCase() === 'educations') ? <EduInfo script={script}/>
